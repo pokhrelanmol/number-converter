c36f0aab843fe0883e21b0b396d0cb95d3107a40
Anmol pokhrel <anmolpokhrel46@gmail.com>
2021-04-11 08:37:09 +0530

    change in modified

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const naturalNumber = document.getElementById("naturalInput");
 const convertB = document.getElementById("convertToBinary");
 const outputB = document.getElementById("binaryOutput");
-
+// function to convert decimal to binary
 const decimalToBinary = (number) => {
   let answer = "";
   let divident = number;
@@ -15,6 +15,7 @@ const decimalToBinary = (number) => {
   answer = divident + answer;
   return answer;
 };
+// addEventListener
 
 convertB.addEventListener("click", () => {
   let userInput = naturalNumber.value;
